61c50c5757e1477337856cb6990a53b613174d52
Anand <anandoncloud99@gmail.com>
2020-08-18 23:29:21 +1000

    biscuit

--- functionmult.js
+++ functionmult.js
@@ -1,4 +1,10 @@
 const functions = {
-    multiply:(num1,num2) => num1 * num2
+    multiply:(num1,num2) => num1 * num2,
+    
+    createUser: () => {
+        const user = {firstname: 'Anand'}
+        user['lastname'] = 'Salimeda';
+        return user;
+    }
 }
     module.exports = functions;
--- functionmult.test.js
+++ functionmult.test.js
@@ -5,3 +5,14 @@ test('multiplies 2*3 to equal 6', () => {
     test('multiplies 2*3 to not equal 5', () => {
         expect(functions.multiply(2,3)).not.toBe(5);
 });
+
+//Test 2
+test('Fitness for police selection is 24 BMI', () => {
+    const weight = 18;
+    const height = 6;
+    expect(weight + height).toBeLessThanOrEqual(24);
+});
+
+test('User should be Anand Salimeda', () => {
+    expect(functions.createUser()).toEqual({firstname: 'Anand', lastname: 'Salimeda'});
+});

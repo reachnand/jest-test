446f394fab7258ef5594419bd94c6673e6dc8d5f
Anand <anandoncloud99@gmail.com>
2020-08-18 22:39:02 +1000

    modified test file

--- functionmult.test.js
+++ functionmult.test.js
@@ -2,3 +2,6 @@ const functions = require('./functionmult');
 test('multiplies 2*3 to equal 6', () => {
     expect(functions.multiply(2,3)).toBe(6);
 });
+    test('multiplies 2*3 to not equal 5', () => {
+        expect(functions.multiply(2,3)).not.toBe(5);
+});
